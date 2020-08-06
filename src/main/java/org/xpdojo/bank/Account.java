1917cff8574bb32a4028dfa1c0a7f4c67b564394
yyap92 <sher_han@list.ru>
2020-08-06 13:56:20 +0300

    adding constructor and static method to create empty account

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -4,6 +4,14 @@ public class Account {
 
     private int balance = 0;
 
+    public Account(int balance) {
+        this.balance = balance;
+    }
+
+    public static Account emptyAccount() {
+        return new Account(0);
+    }
+
     public int getBalance() {
         return balance;
     }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,13 +1,14 @@
 package org.xpdojo.bank;
 
-import org.junit.jupiter.api.Disabled;
+
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.xpdojo.bank.Account.emptyAccount;
 
 public class AccountTest {
 
-    private final Account account = new Account();
+    private final Account account = emptyAccount();
 
     @Test
     public void newAccountShouldHaveZeroBalance(){
