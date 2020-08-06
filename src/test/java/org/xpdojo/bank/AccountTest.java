9304d000f70d4566e222baf243377cf3a35b9333
yyap92 <sher_han@list.ru>
2020-08-06 13:52:15 +0300

    a bit of code rework

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -7,28 +7,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
-    @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
-    }
+    private final Account account = new Account();
 
     @Test
     public void newAccountShouldHaveZeroBalance(){
-        Account account = new Account();
         assertThat(account.getBalance()).isEqualTo(0);
     }
 
     @Test
     public void depositAnAmountShouldIncreaseBalance(){
-        Account account = new Account();
         account.deposit(10);
         assertThat(account.getBalance()).isEqualTo(10);
     }
 
     @Test
     public void multipleDepositAnAmountShouldIncreaseBalanceBySum(){
-        Account account = new Account();
         account.deposit(10);
         account.deposit(10);
         account.deposit(10);
@@ -37,10 +30,10 @@ public class AccountTest {
 
     @Test
     public void multipleWithdrawAnAmmountShouldDecreaseBalanceBySum(){
-        Account account = new Account();
         account.withdraw(10);
         account.withdraw(10);
         assertThat(account.getBalance()).isEqualTo(-20);
     }
+
 
 }
